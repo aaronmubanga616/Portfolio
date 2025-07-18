0161d3daea14071a9045f7d4d9751d82e58ead06
aaronmubanga616 <mubangaa02@gmail.com>
2025-07-18 15:51:34 -0700

    About section final cut personalization v4

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -35,6 +35,32 @@ const About = () => {
           <div className="absolute inset-x-0 pointer-evets-none -bottom-4 h-1/2 sm:h-1/3 bg-gradient-to-t from-indigo" />
         </div>
         {/* Grid 2 */}
+        <div className="grid-default-color grid-5">
+          <div className="z-10 w-[50%]">
+            <p className="headText">Tools/Tech Arsenal</p>
+            <p className="subtext">
+              I specialize in a variety of languages, frameworks, and tools that
+              allow me to build robust and scalable web and cross platform applications
+            </p>
+          </div>
+          <div className="absolute inset-y-0 md:inset-y-9 w-full h-full start-[50%] md:scale-125">
+            <Frameworks />
+          </div>
+        </div>
+        
+        {/* Grid 3 */}
+        <div className="grid-black-color grid-3">
+          <div className="z-10 w-[50%]">
+            <p className="headtext">No borders, Just Great Work. Available Globally </p>
+            <p className="subtext">
+              I'm based in Lusaka, Lusaka, Zambia, and open to remote work worldwide
+            </p>
+          </div>
+          <figure className="absolute left-[30%] top-[10%]">
+            <Globe />
+          </figure>
+        </div>
+        {/* Grid 4 */}
         <div className="grid-default-color grid-2">
           <div
             ref={grid2Container}
@@ -85,19 +111,7 @@ const About = () => {
             />
           </div>
         </div>
-        {/* Grid 3 */}
-        <div className="grid-black-color grid-3">
-          <div className="z-10 w-[50%]">
-            <p className="headtext">No borders, Just Great Work. Available Globally </p>
-            <p className="subtext">
-              I'm based in Lusaka, Lusaka, Zambia, and open to remote work worldwide
-            </p>
-          </div>
-          <figure className="absolute left-[30%] top-[10%]">
-            <Globe />
-          </figure>
-        </div>
-        {/* Grid 4 */}
+        {/* Grid 5 */}
         <div className="grid-special-color grid-4">
           <div className="flex flex-col items-center justify-center gap-4 size-full">
             <p className="text-center headtext">
@@ -105,19 +119,6 @@ const About = () => {
               Let's Connect
             </p>
             <CopyEmailButton />
-          </div>
-        </div>
-        {/* Grid 5 */}
-        <div className="grid-default-color grid-5">
-          <div className="z-10 w-[50%]">
-            <p className="headText">Teck Stack</p>
-            <p className="subtext">
-              I specialize in a variety of languages, frameworks, and tools that
-              allow me to build robust and scalable web and cross platform applications
-            </p>
-          </div>
-          <div className="absolute inset-y-0 md:inset-y-9 w-full h-full start-[50%] md:scale-125">
-            <Frameworks />
           </div>
         </div>
       </div>
