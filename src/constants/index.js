481085b8c8bdd2aa165071a958d8507130297b57
aaronmubanga616 <mubangaa02@gmail.com>
2025-07-18 17:23:46 -0700

    Experience.jsx filling correct details

--- src/constants/index.js
+++ src/constants/index.js
@@ -126,36 +126,27 @@ export const mySocials = [
 
 export const experiences = [
   {
-    title: "Software Developer",
-    job: "Security & Defense Projects",
-    date: "2021-2023",
+    title: "Graphic Designer",
+    job: "Graphic Design Projects",
+    date: "2022-2023",
     contents: [
-      "Enhanced application security and developed new features, adhering to standards set by the Passive Defense Organization and National Cyberspace Center.",
-      "Designed and implemented intuitive map interfaces using MapsUI, enhancing user experience and enabling seamless interactive map integration.",
-      "Developed applications for industrial automation, leveraging C++ and the Fatek API for PLC communication.",
-      "Enhanced responsiveness and usability of applications using Windows Forms and WPF frameworks.",
-      "Executed XML to SVG conversions using X-DOM, ensuring dynamic and efficient data visualization.",
+      "Worked at interact club of Libala as a graphic designer, I was responsible for creating visually engaging content that effectively communicated ideas across both digital and print platforms. My work involved conceptualizing and executing designs that aligned with brand identity and project objectives. I developed a wide range of visual assets including logos, social media content, marketing materials, posters, banners, and user interface mockups. I lead a team of five to understand creative direction, apply design principles, and deliver high-quality results under tight deadlines. Throughout the process, I utilized industry-standard tools and maintained a strong attention to detail, ensuring consistency and visual appeal in every piece.",
     ],
   },
   {
-    title: "Back-End Developer",
-    job: "Car Manufacture",
-    date: "2023-2024",
+    title: "Python Developer",
+    job: "Software Developer",
+    date: "2024-2025",
     contents: [
-      "Engineered systems for large-scale data ingestion and analysis, ensuring efficient data processing and storage.",
-      "Developed back-end systems enabling vehicle-to-cloud communication for telemetry, diagnostics, and remote control:",
-      "✅ Implemented secure APIs, following ISO 26262 automotive safety standards.",
-      "✅ Ensured data privacy for customers and partners through industry-compliant protocols.",
-      "✅ Delivered remote features like over-the-air updates, real-time tracking, and remote start capabilities.",
+      "Over the past year, I've worked as a Python developer with a strong focus on AI and machine learning. My role involved writing clean, efficient Python code to support data processing, model training, and basic automation tasks.",
     ],
   },
   {
-    title: "Freelance Developer",
+    title: "Freelance Developer & Cybersecurity Consultant",
     job: "Self-Employed",
     date: "2025-Present",
     contents: [
-      "Created a personal portfolio using Three.js, React, Vite, and WebAPI to showcase technical expertise.",
-      "Continuously enhancing technical skills and expanding expertise in modern web development and back-end technologies.",
+      "Developed a personal portfolio website utilizing Three.js, React, Vite, and Web APIs to demonstrate technical proficiency and creativity. Actively committed to continuous learning and skill development, with a focus on modern front-end frameworks, web performance optimization, and back-end technologies.",
     ],
   },
 ];
